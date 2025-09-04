6f10d1bd47a5ffeab4ecb670761998c87e5d333a
Saeed <saeedahmed40@protonmail.com>
2025-09-04 15:57:09 +0100

    adding js file

---
+++ rss-feed.js
@@ -0,0 +1,45 @@
+rssfeed_url = new Array();
+rssfeed_url[0] = "https://www.goodreads.com/review/list_rss/179615667?key=BjKdf7EJCMbSQQRZgJnbQq3eEc_kHrIO2eKUAGse76q6L4lN&shelf=%23ALL%23";
+
+rssfeed_frame_width = "230";
+rssfeed_frame_height = "260";
+rssfeed_scroll = "on";
+rssfeed_scroll_step = "6";
+rssfeed_scroll_bar = "off";
+
+rssfeed_font_size = "12";
+rssfeed_font_face = "";
+rssfeed_border = "on";
+
+rssfeed_target = "_blank";
+
+rssfeed_css_url = "https://feed.surfing-waves.com/css/style5.css";
+
+rssfeed_title = "on";
+rssfeed_title_name = "";
+rssfeed_title_bgcolor = "#3366ff";
+rssfeed_title_color = "#fff";
+rssfeed_title_bgimage = "";
+
+rssfeed_footer = "off";
+rssfeed_footer_name = "rss feed";
+rssfeed_footer_bgcolor = "#fff";
+rssfeed_footer_color = "#333";
+rssfeed_footer_bgimage = "";
+
+rssfeed_item_title_length = "50";
+rssfeed_item_title_color = "#666";
+rssfeed_item_bgcolor = "#fff";
+rssfeed_item_bgimage = "";
+rssfeed_item_border_bottom = "on";
+rssfeed_item_source_icon = "off";
+rssfeed_item_date = "off";
+
+rssfeed_item_description = "on";
+rssfeed_item_description_length = "120";
+rssfeed_item_description_color = "#666";
+rssfeed_item_description_link_color = "#333";
+rssfeed_item_description_tag = "off";
+
+rssfeed_no_items = "0";
+rssfeed_cache = "05964451664453e3353beb9046956ae1";
