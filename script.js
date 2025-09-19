2b06407e579c114df7fe5cbfa447a29e5dbbe7bd
Saeed <saeedahmed40@protonmail.com>
2025-09-19 12:51:59 +0100

    seperated navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading the navbar:', error);
+        });
+});
